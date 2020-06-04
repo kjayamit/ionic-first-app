2f682de455f714bcb2e0137404fc889cad960215
Jaya <kjayamit@gmail.com>
2020-06-04 22:33:21 +0530

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+import { TodoService } from '../todo.service';
+import { Todo } from '../todo';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  public todos: Array<Todo> = [];
+  constructor(public todoService: TodoService) {}
+
+  async ngOnInit(){
+
+    this.todos = await this.todoService.read();
+
+  }
+
+  getIcon(todo){
+    if(todo.completed) return 'checkmark-circle';
+    else return 'stopwatch';
+  }
+  public async createTodo(){
+    let key = await this.todoService.generateKey();
+    let todo = {
+      title: `${key}`,
+      note: "A new todo",
+      completed: true
+    };
+    await this.todoService.create(key,todo);
+    this.todos = await this.todoService.read();
+  }
+}
